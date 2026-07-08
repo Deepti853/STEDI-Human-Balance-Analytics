88c17600ff3c0ac4090c0578fc4b4d58e0843cb7
Deepti853 <deeptipandey963@gmail.com>
2026-07-09 03:33:11 +0530

    Add files via upload

---
+++ SQL/customer_landing.sql/customer_landing.sql
@@ -0,0 +1,12 @@
+CREATE EXTERNAL TABLE `customer_landing`(
+)
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://stedi-deepti/customer_landing/'
+TBLPROPERTIES (
+  'classification'='json')
