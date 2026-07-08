c672379cbaf80c97b8e8508cad3a69c17dff25ff
Deepti853 <deeptipandey963@gmail.com>
2026-07-09 03:33:56 +0530

    Add files via upload

---
+++ SQL/accelerometer_landing.sql/accelerometer_landing.sql
@@ -0,0 +1,12 @@
+CREATE EXTERNAL TABLE `accelerometer_landing`(
+)
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://stedi-deepti/accelerometer_landing/'
+TBLPROPERTIES (
+  'classification'='json')
