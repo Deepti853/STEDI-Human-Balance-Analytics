3e17afbc2605332510a34f6f6206ca46111a5117
Deepti853 <deeptipandey963@gmail.com>
2026-07-09 03:32:15 +0530

    Add files via upload

---
+++ SQL/step_trainer_landing.sql/step_trainer_landing.sql
@@ -0,0 +1,12 @@
+CREATE EXTERNAL TABLE `step_trainer_landing`(
+)
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://stedi-deepti/step_trainer_landing/'
+TBLPROPERTIES (
+  'classification'='json')
