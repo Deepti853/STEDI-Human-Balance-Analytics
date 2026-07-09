c4b959124e06fa34cc1f1868f8c8698d6244a1a3
Deepti853 <deeptipandey963@gmail.com>
2026-07-09 11:14:13 +0530

    Define schema for accelerometer_landing table

--- SQL/accelerometer_landing.sql/accelerometer_landing.sql
+++ SQL/accelerometer_landing.sql/accelerometer_landing.sql
@@ -1,4 +1,9 @@
 CREATE EXTERNAL TABLE `accelerometer_landing`(
+  user STRING,
+  timestamp BIGINT,
+  x DOUBLE,
+  y DOUBLE,
+  z DOUBLE
 )
 ROW FORMAT SERDE 
   'org.openx.data.jsonserde.JsonSerDe' 
